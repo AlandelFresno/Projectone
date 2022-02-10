ec8e452f76d7a3c9fb180c4e4716d1452bbebd3f
AlandelFresno <alandelfresno2@gmail.com>
2022-02-10 14:25:10 -0300

    #2
    Header is now navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule
